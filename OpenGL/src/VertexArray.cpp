a1adb007004a6db121ded8fe9981e31d2558756c
EnZzzzzz <957078942@qq.com>
2022-07-17 12:17:05 +0800

    p14 抽象VertexBufferLayout 和 VertexArray

--- OpenGL/src/VertexArray.cpp
+++ OpenGL/src/VertexArray.cpp
@@ -1,1 +1,43 @@
 #include "VertexArray.h"
+#include "Renderer.h"
+
+VertexArray::VertexArray()
+{
+	GLCall(glGenVertexArrays(1, &m_RendererID));
+}
+
+VertexArray::~VertexArray()
+{
+	GLCall(glDeleteVertexArrays(1, &m_RendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer & vb, const VertexBufferLayout & layout)
+{
+	Bind();
+	vb.Bind();
+	const auto& elements = layout.getElement();
+	unsigned int offset = 0;
+	for (unsigned int i = 0; i < elements.size(); i++)
+	{
+		// 告诉OpenGL data里的数据代表什么 https://docs.gl/gl4/glVertexAttribPointer
+		// 第一个index是属性的index，比如定义 位置在index0，纹理在index2，法线在index3
+		// size是该属性有几个数值，比如index 0定义为坐标，坐标有两个float，所以size是2
+		// 用之前要enable这个属性
+		//GLCall(glEnableVertexAttribArray(0));
+		//GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+		const auto& element = elements[i];
+		GLCall(glEnableVertexAttribArray(i));
+		GLCall(glVertexAttribPointer(i, element.count, element.type, element.normalized, layout.getStride(), (const void*) offset));
+		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
+	}
+}
+
+void VertexArray::Bind() const
+{
+	GLCall(glBindVertexArray(m_RendererID));
+}
+
+void VertexArray::Unbind() const
+{
+	GLCall(glBindVertexArray(0));
+}
--- OpenGL/src/VertexArray.h
+++ OpenGL/src/VertexArray.h
@@ -1,15 +1,19 @@
 #pragma once
 
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 
 class VertexArray
 {
 private:
-
+	unsigned int m_RendererID;
 public:
 	VertexArray();
 	~VertexArray();
 
-	//void addBuffer(const VertextBuffer& vb, const VertextBufferLayout& layout);
+	void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+	void Bind() const;
+	void Unbind() const;
 };
 
