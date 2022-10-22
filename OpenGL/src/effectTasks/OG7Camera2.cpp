5f7987ce293e9c4b66b76f206f3205bd1bf7c464
EnZzzzzz <957078942@qq.com>
2022-10-22 21:37:20 +0800

    没改啥

---
+++ OpenGL/src/effectTasks/OG7Camera2.cpp
@@ -0,0 +1,79 @@
+#include "imgui/imgui.h"
+
+#include "OG7Camera2.h"
+#include "VertexBufferLayout.h"
+#include "GLFW/glfw3.h"
+
+
+OG7Camera2::OG7Camera2()
+    :m_ModelVec(glm::vec3(1.0f, 0.0f, 0.0f)), m_ViewVec(glm::vec3(0.0f, 0.0f, -3.0f)), m_Degree(.0f), m_PerspectDegree(45.0f)
+{
+    //开启深度测试
+    glEnable(GL_DEPTH_TEST);
+    float vertices[] = {
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    m_VAO = std::make_unique<VertexArray>();
+    m_VBO = std::make_unique<VertexBuffer>(vertices, sizeof(vertices));
+    m_VBO->bind();
+}
+
+OG7Camera2::~OG7Camera2()
+{
+   
+}
+
+void OG7Camera2::OnRender()
+{
+   
+}
+
+void OG7Camera2::OnImGuiRender()
+{
+    
+}
+
+void OG7Camera2::OnUpdate(float deltaTime)
+{
+}
---
+++ OpenGL/src/effectTasks/OG7Camera2.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Task.h"
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "Renderer.h"
+
+class OG7Camera2 : public Task
+{
+public:
+	OG7Camera2();
+	~OG7Camera2();
+	void OnRender() override;
+	void OnImGuiRender() override;
+	void OnUpdate(float deltaTime) override;
+
+private:
+	std::unique_ptr<VertexArray> m_VAO;
+	std::unique_ptr<VertexBuffer> m_VBO;
+	std::unique_ptr<Shader> m_Shader;
+	std::unique_ptr<Texture> m_Texture;
+	std::unique_ptr<Texture> m_Texture2;
+	std::unique_ptr<Renderer> m_Renderer;
+
+	float m_Degree, m_PerspectDegree;
+	glm::vec3 m_ViewVec;
+	glm::vec3 m_ModelVec;
+
+	glm::mat4 m_Model;
+	glm::mat4 m_Proj;
+	glm::mat4 m_View;
+	glm::mat4 m_MVP;
+};
